83aa086aa5019fbdcc8f007549ffe2d1c1737c93
Miguel Lara <miguel.lara@soytul.com>
2023-07-30 14:27:12 -0500

    Initial commit

---
+++ bin/basic_concepts.dart
@@ -0,0 +1,5 @@
+import 'package:basic_concepts/basic_concepts.dart' as basic_concepts;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basic_concepts.calculate()}!');
+}
---
+++ test/basic_concepts_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_concepts/basic_concepts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
